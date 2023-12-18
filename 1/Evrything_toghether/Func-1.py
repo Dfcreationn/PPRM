d59a6a51fb24f33e8b2d64acdb9d44092183238f
DFcreationn <diheinfernando954@gmail.com>
2023-12-18 16:38:15 +0100

    Version 0.0.0

---
+++ 1/Evrything_toghether/Func-1.py
@@ -0,0 +1,7 @@
+from random import randrange as rn
+from time import sleep as sl
+
+from Variables import f1
+
+
+def f1:
---
+++ 1/Evrything_toghether/Variables.py
@@ -0,0 +1,5 @@
+from random import randrange as rn
+from time import sleep as sl
+
+class f1:
+    input1 = input("Hello choose a number between 1 - 999")
